459861338ab8378b6b7aa5f0df8b2f1df844defc
a7md.abdullagit config --global user.email a7md.abdulla03@gmail.com <a7md.abdulla03@gmail.com>
2025-04-27 21:50:50 +0300

    JS-Functions

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-// Ex.1-maxOfTwoNumbers()
-const maxOfTwoNumbers = (x, y) => {
-    if (x >= y){
-        return x
-    } else {
-        return y
-    }
-}
-console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+/*
+Exercise 1: maxOfTwoNumbers()
 
-// Ex.2-isAdult()
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
 const isAdult = (age) => {
     ageGroup = ""
     if (age >= 18) {
@@ -20,7 +40,18 @@ const isAdult = (age) => {
 }
 console.log('Exercise 2 Result:', isAdult(21))
 
-// Ex.3-isCharAVowel()
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
 const isCharAVowel = (char) => {
     char = char.toLowerCase()
     if (char === 'a' || char === 'e' || char === 'o' || char === 'u' || char === 'i') {
@@ -31,19 +62,48 @@ const isCharAVowel = (char) => {
 }
 console.log('Exercise 3 Result:', isCharAVowel("a"));
 
-// Ex.4-generateEmail()
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
 const generateEmail = (name, domain) => {
     return name + "@" + domain
 }
 console.log('Exercise 4 Result:',generateEmail('ahmed', 'gmail.com'))
 
-// Ex.5 greetUser()
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
 const greetUser = (name, dayTime) => {
     return "Good " + dayTime + ", " + name +"!"
 }
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
 
-// Ex.6 maxOfThree()
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
 const maxOfThree = (n1, n2, n3) => {
     if (n1 >= n2 && n1 >= n3){
         return n1
@@ -55,13 +115,35 @@ const maxOfThree = (n1, n2, n3) => {
 }
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
-// Ex.7-calculateTip()
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
 const calculateTip = (billAmount, tipPercentage) => {
     return billAmount*(tipPercentage/100)
 }
 console.log('Exercise 7 Result:', calculateTip(50, 20));
 
-// Ex.8-convertTemperature()
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
 const convertTemperature = (temp, tempScaleUnit) => {
     if (tempScaleUnit === 'C') {
         return (temp * 9/5) + 32 + ' (Fahrenheit)'
@@ -73,7 +155,21 @@ const convertTemperature = (temp, tempScaleUnit) => {
 }
 console.log('Exercise 8 Result:', convertTemperature(32, "C"));
 
-// Ex.9-basicCalculator()
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
 const basicCalculator = (num1, num2, opr) => {
     if (opr === 'add'){
         return num1 + num2
